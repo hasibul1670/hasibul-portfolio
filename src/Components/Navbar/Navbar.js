6088f2cdf485f243599f8aa989c2e0b225977e76
hasibul1670 <hasibulislam1670@gmail.com>
2023-07-04 14:47:52 +0600

    ddd

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,46 +1,70 @@
-import React from 'react'
-import { Link } from 'react-scroll';
+import React from "react";
+import { Link } from "react-scroll";
 import "./Navbar.css";
 
 function Navbar() {
-    return (
-     <div id="home" className="  home">
-                
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container container-fluid">
-      <Link class="navbar-brand font-weight-bold " href="#">Hasibul Islam</Link> 
+  return (
+    <div id="home" className="  home">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container container-fluid">
+          <Link class="navbar-brand font-weight-bold " href="#">
+            Hasibul Islam
+          </Link>
 
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="icon-bar top-bar"></span>
-          <span class="icon-bar middle-bar"></span>
-          <span class="icon-bar bottom-bar"></span>
-          <span class="sr-only">Toggle navigation</span>
-        </button>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="icon-bar top-bar"></span>
+            <span class="icon-bar middle-bar"></span>
+            <span class="icon-bar bottom-bar"></span>
+            <span class="sr-only">Toggle navigation</span>
+          </button>
 
-      <div className=" collapse navbar-collapse text-center" id="navbarResponsive">
-          <ul className="navbar-nav ml-auto">
-          <li className="nav-right active mx-2">
-              <a className="nav-link text-white " href="#home">Home</a>
-            </li>
-            <li className="nav-right active mx-2">
-              <a className="nav-link text-white " href="#project">Projects</a>
-            </li>
-            <li className="nav-right active mx-2">
-              <a className="nav-link text-white " href="#blog">Blogs</a>
-            </li>
-            <li className="nav-item active mx-2">              
-            <a className="nav-link text-white" href="#about">About</a>
-            </li>
-         
-               <a className="nav-link text-white btn btn-danger px-3 text-white" target="_blank" href="https://drive.google.com/file/d/1GzHjeyR_Ra1e7NsyKxCs291OCEUfmTjK/view?usp=sharing">Resume
-               </a>  
+          <div
+            className=" collapse navbar-collapse text-center"
+            id="navbarResponsive"
+          >
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-right active mx-2  nav-link nav-link-ltr">
+                <a className="nav-link text-white " href="#home">
+                  Home
+                </a>
+              </li>
+              <li className="nav-right active mx-2  nav-link nav-link-ltr">
+                <a className="nav-link text-white " href="#project">
+                  Projects
+                </a>
+              </li>
+              <li className="nav-right active mx-2  nav-link nav-link-ltr">
+                <a className="nav-link text-white " href="#blog">
+                  Blogs
+                </a>
+              </li>
+              <li className="nav-right active mx-2  nav-link nav-link-ltr">
+                <a className="nav-link text-white" href="#about">
+                  About
+                </a>
+              </li>
 
-          </ul>
+              <a
+                className="nav-link text-white btn-sm btn btn-danger text-white"
+                target="_blank"
+                href="https://drive.google.com/file/d/12xW5IOLqh1WVMBaArCck4M4HlXyopI-T/view?usp=sharing"
+              >
+                Resume
+              </a>
+            </ul>
+          </div>
         </div>
+      </nav>
     </div>
-  </nav>
-        </div>
-    )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,98 +1,104 @@
-import React from 'react'
-import creative from '../../images/projectImg/cre.png';
-import traval from '../../images/projectImg/travle.png';
-import vol from '../../images/projectImg/vol.png';
-import app from '../../images/projectImg/app.png';
-
-
+import React from "react";
+import ejournal from "../../images/projectImg/ejournal.png";
+import sunlight from "../../images/projectImg/sunlight.png";
 
 function Project() {
-    return (
-        <div id="project" className='container'>
-            
-            <div class=" pt-4 pb-4 title d-flex justify-content-center align-items-center">
-                    <h2>My Projects</h2>
-                </div>
-                
-          <div class="card-deck">
-              <div className="col-9 col-sm-9 col-md-6 col-lg-6 col-xl-4">
-
-           
-  <div class="card">
-    <img className="      img-fluid card-img-top" src={creative} alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title text-center text text-white">Creative Agency Website</h5>
-      <p class="card-text text-center text-white ">A fullstack Agency website using React,Firebase Authentication,Bootstrap,MongoDB and NodeJs</p>
-
-      <div className="text-center">
-      <a href='https://creative-agency365.netlify.app/' class="btn1 btn  ">Website</a> 
-      <a href="https://github.com/hasibul1670/creative-agency-client" class="btn2 btn  ">Github</a>
+  return (
+    <div id="project" className="container">
+      <div class=" pt-4 pb-4 title d-flex justify-content-center align-items-center">
+        <h2>My Projects</h2>
       </div>
 
-    </div>
-  </div>
-  
-  </div>
-  
+      <div class="card-deck">
+        <div className="col-9 col-sm-9 col-md-6 col-lg-5 col-xl-4">
+          <div class="card ">
+            <img
+              className=" p-2 img-fluid card-img-top"
+              src={sunlight}
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center text text-white">
+                SunLight Academy
+              </h5>
+              <p class="card-text text-center text-white ">
+                A fullstack E-learning website using React,Tailwind CSS,JWT
+                NodeJs,MongoDB,etc
+              </p>
 
-  <div className='col-9 col-sm-9 col-md-6 col-lg-6 col-xl-4'>
-  <div class="card">
-    <img  className="      img-fluid card-img-top" src={app}   alt="..."/>
-    <div class="card-body">
-    <h5 class="card-title text-center text text-white">Apartment Hunt Website</h5>
-  
-      <p class="card-text text-center text-white ">A fullstack Apartment agency website using React,Redux,Firebase Authentication,Bootstrap,MongoDB and NodeJs</p>
+              <div className="text-center">
+                <a
+                  target="blank"
+                  href="https://summer-school-camp.netlify.app/"
+                  className="btn1 btn btn-sm mb-2"
+                >
+                  Website
+                </a>
 
-      <div className="text-center">
-      <a href="https://team1-apartment-hunt.netlify.app/" class="btn1 btn  ">Website</a> 
-      <a href="https://github.com/hasibul1670/Apartment-Hunt" class="btn2 btn  ">Github</a>
-      </div>
-
-    </div>
-  </div>
-</div>
-
-
-
-  <div className='col-9 col-sm-9 col-md-6 col-lg-6 col-xl-4'>
-  <div class="card">
-    <img className="      img-fluid card-img-top" src={vol}  alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title text-center text text-white">Volunteer Network Website </h5>
-      <p class="card-text text-center text-white ">A Volunteer Website using React,Firebase Authentication,Bootstrap,HTML5, Netlify,CSS3</p>
-
-      <div className="text-center">
-      <a href="https://volunteer-network365.netlify.app/" class="btn1 btn  ">Website</a> 
-      <a href="https://github.com/hasibul1670/volunteer-network" class="btn2 btn  ">Github</a>
-      </div>
-
-    </div>
-  </div>
-</div>
-
-
-<div className='col-9 col-sm-9 col-md-6 col-lg-6 col-xl-4'>
-  <div class="card">
-    <img className="      img-fluid card-img-top" src={traval}   alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title text-center text text-white">Travel Agency Website</h5>
-      <p class="card-text text-center text-white ">A Travel Agency Website using React,Firebase Authentication,Bootstrap,HTML5,CSS3,Netlify</p>
-
-      <div className="text-center">
-      <a href="https://travel-guru-365.netlify.app/" class="btn1 btn  ">Website</a> 
-      <a href="https://github.com/hasibul1670/travel-guru" class="btn2 btn  ">Github</a>
-      </div>
-
-    </div>
-  </div>
-  </div>
-
-
-
- 
-</div>
+                <a
+                  target="_blank"
+                  href="https://github.com/hasibul1670/Client-Summer-Camp-School"
+                  className="btn1 btn btn-sm mb-2"
+                >
+                  Github Front-End
+                </a>
+                <a
+                  target="_blank"
+                  href="https://github.com/hasibul1670/summer-camp-school-server"
+                  className="btn1 btn btn-sm mb-2"
+                >
+                  Github Back-End
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+        <div className="col-9 col-sm-9 col-md-6 col-lg-5 col-xl-4">
+          <div class="card ">
+            <img
+              className=" p-2 img-fluid card-img-top"
+              src={ejournal}
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title text-center text text-white">
+             Electronic Journal
+              </h5>
+              <p class="card-text text-center text-white ">
+                A fullstack E-Journal website using React,BootStarp,JWT
+                NodeJs,MongoDB,etc
+              </p>
+
+              <div className="text-center">
+                <a
+                  target="_blank"
+                  href="https://electronic-journal-client.vercel.app/"
+                  className="btn1 btn btn-sm mb-2"
+                >
+                  Website
+                </a>
+
+                <a
+                  target="_blank"
+                  href="https://github.com/hasibul1670/Electronic-Journal-client"
+                  className="btn1 btn btn-sm mb-2"
+                >
+                  Github Front-End
+                </a>
+                <a
+                  target="_blank"
+                  href="https://github.com/hasibul1670/Electronic-Journal-Server"
+                  className="btn1 btn btn-sm mb-2"
+                >
+                  Github Back-End
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Project
+export default Project;
