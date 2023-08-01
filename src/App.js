13d9582d8ec30aa04663a23f84a3136cb3ad90e2
hasibul1670 <hasibulislam1670@gmail.com>
2023-08-02 00:16:02 +0600

    CD : deployment error Fixed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-  
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-
-
-import './App.css';
+import "./App.css";
 import Home from "./Components/Home/Home";
-import Project from "./Components/Project/Project";
 import NoMatch from "./Components/NoMatch.js";
-
+import Project from "./Components/Project/Project";
 
 function App() {
   return (
-    <div >
-      
-    <Router>
-      <Switch>
-      <Route exact path="/"><Home /></Route>
-      <Route path="/project"><Project></Project></Route>
-      <Route path="*">
-            <NoMatch/>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
           </Route>
-    
-      </Switch>
-    </Router>
+          <Route path="/project">
+            <Project></Project>
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
